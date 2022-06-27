145473dfbb6409bba8c1916f801770a362c2b83d
Yifan <datasciencewyfan@gmail.com>
2022-06-27 11:14:55 -0700

    commit tree 8c5d7dab57cbaa3c6e92d0fb37c62f192f7ac93a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+
+class Anagram
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(list)
+        list.filter { |candidate| @word.split("").sort_by { |letter| letter}.join("") == candidate.split("").sort_by { |letter| letter}.join("") }
+    end
+end
